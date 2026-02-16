b3e6832ba00b76b54f6f6577d294a6b9ac4c2cab
Egor Kolesnikov <99754312+EgorBiophys@users.noreply.github.com>
2026-02-16 15:54:39 -0500

    Add reference to nucleosome.crd in equil step

--- Nucleosome_simulation_files/simulation.sh
+++ Nucleosome_simulation_files/simulation.sh
@@ -4,7 +4,7 @@ pmemd.cuda -O -i min.in -o min.out -p nucleosome.top -c nucleosome.crd -r min.nc
 
 pmemd.cuda -O -i heat.in -o heat.out -p nucleosome.top -c min.ncrst -r heat.ncrst -x heat.nc -inf heat.mdinfo -ref nucleosome.crd
 
-pmemd.cuda -O -i equil.in -o equil.out -p nucleosome.top -c heat.ncrst -r equil.ncrst -x equil.nc -inf equil.mdinfo
+pmemd.cuda -O -i equil.in -o equil.out -p nucleosome.top -c heat.ncrst -r equil.ncrst -x equil.nc -inf equil.mdinfo -ref nucleosome.crd
 
 pmemd.cuda -O -i prod.in -o prod.out -p nucleosome.top -c equil.ncrst -r prod.ncrst -x prod.nc -inf prod.mdinfo
 
